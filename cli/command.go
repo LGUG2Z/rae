d92321c8433cba5b9fbfe86a4e79dae270bf01f5
jiqb <gthbji@ml1.net>
2018-09-30 11:37:19 +0100

    Rename generator.go -> command.go and handle dynamic verb completions

--- cli/command.go
+++ cli/command.go
@@ -7,6 +7,9 @@ import (
 	"sort"
 
 	"github.com/urfave/cli"
+	"io/ioutil"
+	"path"
+	"gopkg.in/yaml.v2"
 )
 
 func GenerateRecipeCommand(recipe *Recipe, c *Config, envVars []string) cli.Command {
@@ -67,12 +70,59 @@ func GenerateContextCommand(context *Context, c *Config, envVars []string) cli.C
 	}
 }
 
+func appendCompletions(file string, completions []string) ([]string, error) {
+	composeFile := map[interface{}]interface{}{}
+	bytes, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := yaml.Unmarshal(bytes, composeFile); err != nil {
+		return nil, err
+	}
+
+	for service, _ := range composeFile["services"].(map[interface{}]interface{}) {
+		completions = append(completions, service.(string))
+	}
+
+	return completions, nil
+}
+
 func GenerateVerbCommand(verb *Verb, c *Config, envVars []string) cli.Command {
 	return cli.Command{
 		Name:        verb.Name,
 		Description: verb.Description,
 		Usage:       verb.Usage,
 		Category:    verb.Category,
+		BashComplete: func(ctx *cli.Context) {
+			var completions []string
+			var err error
+
+			context := strings.Split(ctx.Command.FullName(), " ")
+			if context[0] == "global" {
+				var files []string
+				if context[0] == "global" {
+					for _, context := range c.Contexts {
+						if context.Name != "global" && context.Name != "recipe" {
+							files = append(files, path.Join(c.Home, fmt.Sprintf("%s.yaml", context.Name)))
+						}
+					}
+				}
+
+				for _, file := range files {
+					completions, err = appendCompletions(file, completions)
+					if err != nil {
+						panic(err)
+					}
+				}
+			} else {
+				file := path.Join(c.Home, fmt.Sprintf("%s.yaml", context[0]))
+				completions, err = appendCompletions(file, completions)
+			}
+
+
+			fmt.Fprintf(ctx.App.Writer, strings.Join(completions, " "))
+		},
 		Action: func(ctx *cli.Context) error {
 			var composeFiles []string
 			var err error
